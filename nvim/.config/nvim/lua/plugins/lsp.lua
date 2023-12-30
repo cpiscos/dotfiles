5d26e5ce2a0bac9c7fb49f189bc6aeaadfcde05a
cpiscos <christian.piscos@gmail.com>
2023-12-30 03:28:35 -0800

    nvim: rust lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -84,4 +84,25 @@ return {
       })
     end
   },
+  {
+    'simrat39/rust-tools.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    config = function()
+      local rt = require('rust-tools')
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover" })
+            vim.keymap.set("n", "<leader>la", rt.code_action_group.code_action_group,
+              { buffer = bufnr, desc = "Code Actions" })
+            vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = bufnr, desc = "Format" })
+          end,
+        },
+      })
+    end
+  }
 }
