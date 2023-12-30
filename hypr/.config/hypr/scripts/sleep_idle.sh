88ba37bcef9b5ae6807d1d4f7fa154b85d359096
cpiscos <christian.piscos@gmail.com>
2023-12-30 03:27:11 -0800

    swayidle script

---
+++ hypr/.config/hypr/scripts/sleep_idle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEFAULT_IDLE_TIME=300
+
+swayidle_run() {
+	idle_time="${1:-$DEFAULT_IDLE_TIME}"
+	swayidle -w timeout "$idle_time" "hyprctl dispatch dpms off" \
+		resume "hyprctl dispatch dpms on" \
+		timeout "$idle_time" "ags -q" \
+		resume "ags"
+}
+
+if [[ "$1" == "--dry" ]]; then
+	swayidle_run 5
+else
+	swayidle_run &
+fi
