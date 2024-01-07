6f2025e7f61d478ebb7c28ee5f753b796100650f
cpiscos <christian.piscos@gmail.com>
2024-01-06 21:07:20 -0800

    remove ags from sleep_idle

--- hypr/.config/hypr/scripts/sleep_idle.sh
+++ hypr/.config/hypr/scripts/sleep_idle.sh
@@ -6,8 +6,9 @@ swayidle_run() {
 	idle_time="${1:-$DEFAULT_IDLE_TIME}"
 	swayidle -w timeout "$idle_time" "hyprctl dispatch dpms off" \
 		resume "hyprctl dispatch dpms on" \
-		timeout "$idle_time" "ags -q" \
-		resume "ags"
+  >> /tmp/sleep_idle.log 2>&1
+		# timeout "$idle_time" "ags -q" \
+		# resume "ags" \
 }
 
 if [[ "$1" == "--dry" ]]; then
