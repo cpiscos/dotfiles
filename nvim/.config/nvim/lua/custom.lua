6e133781a83413f0712ae03a3e98c5158ea78f2a
cpiscos <christian.piscos@gmail.com>
2024-03-19 02:08:59 -0700

    nvim: resize split mode

--- nvim/.config/nvim/lua/custom.lua
+++ nvim/.config/nvim/lua/custom.lua
@@ -6,6 +6,7 @@ vim.api.nvim_set_keymap('n', '<leader>bp', ':bp<CR>', { noremap = true, silent =
 vim.api.nvim_set_keymap('n', '<leader>bl', ':ls<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>bs', ':ls<CR>:b<Space>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>d', ':q<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ts', '', {
   noremap = true,
   silent = true,
@@ -14,31 +15,40 @@ vim.api.nvim_set_keymap('n', '<leader>ts', '', {
   end
 })
 
+vim.api.nvim_set_keymap('n', '<leader>tg', '', {
+  noremap = true,
+  silent = true,
+  callback = function()
+    vim.fn.system('lazygit &')
+  end
+})
+
 vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
+  pattern = '*',
   callback = function()
     if #vim.fn.argv() == 0 and vim.bo.filetype ~= 'help' then
-      vim.cmd("Neotree reveal")
+      vim.cmd('Neotree reveal')
     end
   end
 })
 
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
   callback = function()
-    vim.cmd("startinsert")
+    vim.cmd('startinsert')
+    vim.cmd('setlocal nonumber norelativenumber')
   end
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*',
   callback = function()
     if vim.bo.buftype == 'terminal' then
-      vim.cmd("startinsert")
+      vim.cmd('startinsert')
 
-      -- local esc_key = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+      -- local esc_key = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
       -- vim.defer_fn(function()
-      --   vim.api.nvim_feedkeys(esc_key, "n", true)
+      --   vim.api.nvim_feedkeys(esc_key, 'n', true)
       -- end, 100)
     end
   end
@@ -49,7 +59,7 @@ vim.api.nvim_set_keymap('n', '<leader>yy', '"+y', { noremap = true, silent = tru
 vim.api.nvim_set_keymap('v', '<leader>yp', '"+p', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>yp', '"+p', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('t', '<leader><ESC>', '<C-\\><C-n>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('t', '<leader><ESC>', '<C-\\><C-n>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<leader>tt', '<C-\\><C-n>:ToggleTerm<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tt', '', {
   noremap = true,
@@ -70,3 +80,35 @@ vim.api.nvim_set_keymap('n', '<leader>tt', '', {
   end
 })
 vim.api.nvim_set_keymap('t', '<leader>lr', ':LspRestart<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<C-w>r', '', {
+  noremap = true,
+  silent = true,
+  callback = function()
+    local resize_mappings = {
+      h = function() vim.cmd('vertical resize -1') end,
+      j = function() vim.cmd('resize +1') end,
+      k = function() vim.cmd('resize -1') end,
+      l = function() vim.cmd('vertical resize +1') end,
+    }
+
+    for key, action in pairs(resize_mappings) do
+      vim.api.nvim_set_keymap('n', key, '', {
+        noremap = true,
+        silent = true,
+        callback = action
+      })
+    end
+
+    vim.api.nvim_set_keymap('n', '<ESC>', '', {
+      noremap = true,
+      silent = true,
+      callback = function()
+        for key, _ in pairs(resize_mappings) do
+          vim.api.nvim_del_keymap('n', key)
+        end
+        vim.api.nvim_del_keymap('n', '<ESC>')
+      end
+    })
+  end
+})
